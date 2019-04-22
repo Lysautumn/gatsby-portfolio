33327b4b3709afb6ee84272756df965a0642c439
Ally <ally@primeacademy.io>
2019-04-22 16:26:28 -0500

    contact links

--- gatsby-portfolio/src/pages/contact.js
+++ gatsby-portfolio/src/pages/contact.js
@@ -23,40 +23,48 @@ class Contact extends Component {
       <Layout>
       <div className="contentSpacing">
         <h1 className="headers">Contact Me</h1>
-        <form className="contactForm">
-          <TextField
-            required
-            id="contactFirstName"
-            label="First Name"
-            value={this.state.firstName}
-            fullWidth
-          />
-          <TextField
-            required
-            id="contactLastName"
-            label="Last Name"
-            value={this.state.lastName}
-            fullWidth
-          />
-          <TextField
-            required
-            id="contactEmail"
-            label="Email"
-            value={this.state.email}
-            fullWidth
-          />
-          <TextField
-            required
-            id="contactMessage"
-            label="Message"
-            value={this.state.message}
-            multiline
-            rows="4"
-            fullWidth
-          />
-          <Button style={styles.button} variant="contained">Submit</Button>
-        </form>
-  
+        <div className="contactContent">
+          <div className="contactLinks">
+            <p className="contactItem"><span class="lnr lnr-envelope"> </span><a href="mailto:alyssaboyd85@gmail.com">Email</a></p>
+            <p className="contactItem"><span class="lnr lnr-link"> </span><a target="_blank" href="https://www.linkedin.com/in/alyssaboyd">LinkedIn</a></p>
+            <p className="contactItem"><span class="lnr lnr-code"> </span><a target="_blank" href="https://www.github.com/lysautumn">GitHub</a></p>
+            <p className="contactItem"><span class="lnr lnr-bubble"> </span><a target="_blank" href="https://www.twitter.com/allyautumnboyd">Twitter</a></p>
+            <p className="contactItem"><span class="lnr lnr-picture"> </span><a target="_blank" href="https://www.dribbble.com/allyboyd">Dribbble</a></p>
+          </div>
+          <form className="contactForm">
+            <TextField
+              required
+              id="contactFirstName"
+              label="First Name"
+              value={this.state.firstName}
+              fullWidth
+            />
+            <TextField
+              required
+              id="contactLastName"
+              label="Last Name"
+              value={this.state.lastName}
+              fullWidth
+            />
+            <TextField
+              required
+              id="contactEmail"
+              label="Email"
+              value={this.state.email}
+              fullWidth
+            />
+            <TextField
+              required
+              id="contactMessage"
+              label="Message"
+              value={this.state.message}
+              multiline
+              rows="4"
+              fullWidth
+            />
+            <Button style={styles.button} variant="contained">Submit</Button>
+          </form>
+        </div>
       </div>
     </Layout>
 
