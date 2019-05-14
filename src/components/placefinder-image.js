078ba8df4b8353c38f7225f40b4f0de9822962d4
Ally <ally@primeacademy.io>
2019-05-14 09:53:55 -0500

    adds work section

---
+++ src/components/placefinder-image.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.app/gatsby-image
+ * - `StaticQuery`: https://gatsby.app/staticquery
+ */
+
+const PlacefinderImage = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "minneapolis-placefinder.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 150) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} className="workImage"/>}
+  />
+)
+export default PlacefinderImage;
+
+
+
+
+
--- src/pages/my-work.js
+++ src/pages/my-work.js
@@ -2,11 +2,38 @@
 
 import React from 'react';
 import Layout from '../components/layout';
-
+import Design1Image from '../components/design-1-image';
+import Design2Image from '../components/design-2-image';
+import HelpTrackerImage from '../components/help-tracker-image';
+import PlacefinderImage from '../components/placefinder-image';
 
 const MyWork = () => (
   <Layout>
-      
+    <div className="contentSpacing">
+      <h1 className="headers">My Work</h1>
+      <div className="workContent">
+        <div className="workCard">
+          <p className="workCardTitle">Help Tracker</p>
+          <p className="workCardDescription">A tool used at Prime to track student attendance at help sessions</p>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lysautumn/help_tracker"><HelpTrackerImage /></a>
+        </div>
+        <div className="workCard">
+          <p className="workCardTitle">Minneapolis Placefinder</p>
+          <p className="workCardDescription">An app I built to explore the Foursquare API</p>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Lysautumn/placefinder"><PlacefinderImage /></a>
+        </div>
+        <div className="workCard">
+          <p className="workCardTitle">Daily UI Day 3: "Above the Fold"</p>
+          <p className="workCardDescription">A design challenge to create a landing page, above the fold</p>
+          <a target="_blank" rel="noopener noreferrer" href="https://dribbble.com/shots/5871283-Daily-UI-003"><Design1Image /></a>
+        </div>
+        <div className="workCard">
+          <p className="workCardTitle">Daily UI Day 4: "Calculator"</p>
+          <p className="workCardDescription">A design challenge to create a calculator, I created light and dark modes</p>
+          <a target="_blank" rel="noopener noreferrer" href="https://dribbble.com/shots/5902744-Daily-UI-004"><Design2Image /></a>
+        </div> 
+      </div>
+    </div>
   </Layout>
 
 )
